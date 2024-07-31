4398198182334c251c5903eca5faac430ad6fa48
Salome Gogritchiani <81165724+salosalo123@users.noreply.github.com>
2024-07-31 15:27:57 +0400

    crate pool section and popular brens section with there html file and js file

--- footer.js
+++ footer.js
@@ -1,9 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  loadContent("footer.html", "footer");
-  loadContent("footer.html", "pool_section");
+  loadContent("pool.html", "pool_section");
+  loadContent1("footer.html", "footer");
+  loadContent2("popularbrends.html", "popularbrend");
 });
 
+function loadContent1(page, elementId) {
+  fetch(page)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(elementId).innerHTML = data;
+    });
+}
+
 function loadContent(page, elementId) {
+  fetch(page)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(elementId).innerHTML = data;
+    });
+}
+
+function loadContent2(page, elementId) {
   fetch(page)
     .then((response) => response.text())
     .then((data) => {
